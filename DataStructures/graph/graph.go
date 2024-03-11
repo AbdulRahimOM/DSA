14ff90ac0b5efb97b4c1ca1051fd5c433c287c6c
Abdul Rahim Ovingal <abdulearningit@gmail.com>
2024-03-11 10:28:40 +0530

    graph (BFS and DFS)

--- DataStructures/graph/graph.go
+++ DataStructures/graph/graph.go
@@ -4,35 +4,62 @@ import (
 	"fmt"
 )
 
-type Graph struct {
-	adjacency map[int][]int
+type Graph map[int][]int
+
+func (graph *Graph) BFS(start int) {
+
+	queue := []int{start}
+	visited := map[int]bool{start: true}
+
+	for len(queue) > 0 {
+		fmt.Println("Visited :", queue[0])
+		for _, neighbor := range (*graph)[queue[0]] {
+			if !visited[neighbor] {
+				visited[neighbor] = true
+				queue = append(queue, neighbor)
+			}
+		}
+		queue = queue[1:]
+	}
 }
 
-func NewGraph() *Graph {
-	return &Graph{
-		adjacency: make(map[int][]int),
+func (graph *Graph) DFS(node int){
+	graph.dfs(node,make(map[int]bool))
+}
+
+func (graph *Graph) dfs(node int, visited map[int]bool) {
+	visited[node]=true
+	fmt.Println("Visited node:", node)
+	for _,relative:=range (*graph)[node]{
+		if !visited[relative]{
+			graph.dfs(relative,visited)
+		}
 	}
+}
+func NewGraph() *Graph {
+	graph := make(Graph)
+	return &graph
 }
 
 func (graph *Graph) AddVertex(value int) error {
-	if _, ok := graph.adjacency[value]; ok {
+	if _, ok := (*graph)[value]; ok {
 		return fmt.Errorf("vertex already exists")
 	}
-	graph.adjacency[value] = []int{}
+	(*graph)[value] = []int{}
 	return nil
 }
 
 func (graph *Graph) AddEdge(from, to int) error {
-	if _, ok := graph.adjacency[to]; !ok {
+	if _, ok := (*graph)[to]; !ok {
 		return fmt.Errorf("to vertex doesn't exists")
 	}
-	if edges, ok := graph.adjacency[from]; !ok {
+	if edges, ok := (*graph)[from]; !ok {
 		return fmt.Errorf("from vertex doesn't exists")
 	} else {
 		if contains(edges, to) {
 			return fmt.Errorf("edge already exist")
 		} else {
-			graph.adjacency[from] = append(edges, to)
+			(*graph)[from] = append(edges, to)
 		}
 	}
 	return nil
@@ -47,7 +74,8 @@ func contains(slice []int, key int) bool {
 }
 
 func (graph *Graph) PrintGraph() {
-	for i,v:=range graph.adjacency{
-		fmt.Printf("Vertex %d: %v\n",i,v)
+	for i, v := range *graph {
+		fmt.Printf("Vertex %d: %v\n", i, v)
 	}
+	fmt.Println()
 }
--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
-import (
-	"DSA/test"
-)
+import "DSA/test"
 
 func main() {
 	// // test.TestTrie()
 	test.TestGraph()
+	// test.TestHeapSort()
 }
